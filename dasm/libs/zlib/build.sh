1f885189aec9de07aec43c832b3bade88f23687b
ryuukk <ryuukk.dev@gmail.com>
2021-12-04 02:11:58 +0100

    improve zlib build script

--- dasm/libs/zlib/build.sh
+++ dasm/libs/zlib/build.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-PATH_ZLIB=
+PATH_ZLIB=/home/ryuukk/tmp/zlib/
 
-clang -c -emit-llvm --target=wasm32 -nostdlib -nostdinc -DZ_SOLO -Izlib/  zlib/adler32.c zlib/crc32.c zlib/deflate.c  zlib/infback.c  zlib/inffast.c  zlib/inflate.c  zlib/inftrees.c  zlib/trees.c  zlib/zutil.c
+mkdir -p build/
+cd build/
+
+clang -c -emit-llvm --target=wasm32 -nostdlib -nostdinc -DZ_SOLO \
+    -I$PATH_ZLIB          \
+    $PATH_ZLIB/adler32.c  \
+    $PATH_ZLIB/crc32.c    \
+    $PATH_ZLIB/deflate.c  \
+    $PATH_ZLIB/infback.c  \
+    $PATH_ZLIB/inffast.c  \
+    $PATH_ZLIB/inflate.c  \
+    $PATH_ZLIB/inftrees.c \
+    $PATH_ZLIB/trees.c    \
+    $PATH_ZLIB/zutil.c    \
 
 llvm-link -o zlib.bc adler32.bc crc32.bc deflate.bc  infback.bc  inffast.bc  inflate.bc  inftrees.bc  trees.bc  zutil.bc
 
 llvm-ar rv zlib.a zlib.bc
+
+cd ../
