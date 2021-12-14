a0907597f1e592ce614d6032df5268bdf6942296
ryuukk <ryuukk.dev@gmail.com>
2021-12-14 02:53:54 +0100

    improve build script

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-echo "Building.."
+BUILD=debug
+OPT=false
+OUTPUT=bin/game.wasm # for opt build
+
+echo "Building for WASM in '$BUILD' mode opt: '$OPT' os: '$OSTYPE'" 
 cd dasm/game
 
-# dub build -c wasm --compiler=ldc2 --arch=wasm32-unknown-unknown-wasm -b release
-dub build -c wasm --compiler=ldc2 --arch=wasm32-unknown-unknown-wasm
+dub build -c wasm --compiler=ldc2 --arch=wasm32-unknown-unknown-wasm -b $BUILD
 
 cd ../..
 
-# echo "Optimizing wasm file for size.."
-# wasm-opt -Oz -o bin/game.wasm bin/game.wasm
+if [[ $OPT == true ]]; then
+    echo "Optimizing wasm file for size.."
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        wasm-opt -Oz -o bin/game.wasm bin/game.wasm
+    elif [[ "$OSTYPE" == "win32" ]]; then
+        C:/emsdk/upstream/bin/wasm-opt -Oz -o $OUTPUT bin/game.wasm
+    elif [[ "$OSTYPE" == "msys" ]]; then
+        # TODO: use package manager's path
+        C:/emsdk/upstream/bin/wasm-opt -Oz -o $OUTPUT bin/game.wasm
+    fi
+fi
